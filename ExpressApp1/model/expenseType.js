fff398f86bc81f63adfac26933c833f2b89df380
prashant.tyagi <ptyagi76@gmail.com>
2017-06-13 23:02:54 +0530

    Added expenseType

---
+++ ExpressApp1/model/expenseType.js
@@ -0,0 +1,16 @@
+﻿var connection = require('../data_repositories/connection');
+var Sequelize = require("sequelize");
+sequelize = connection.getConnection();
+
+var Models = {};
+const ExpenseType = sequelize.define('ExpenseType', {
+    Name: { type: Sequelize.STRING, allowNull: false },
+    Detail: { type: Sequelize.STRING, allowNull: true },
+    ExpenseTypeId: { type: Sequelize.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true }
+});
+ExpenseType.sync().then(() => {
+    // Table created
+    console.log('Expense Type Created');
+});
+Models.ExpenseGroup = ExpenseGroup;
+module.exports = Models;
