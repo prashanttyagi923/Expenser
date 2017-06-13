cfc0bd757e442c3412538d9f33238729e8ec3d05
prashant.tyagi <ptyagi76@gmail.com>
2017-06-13 18:33:55 +0530

    Added expenseGroupUser

---
+++ ExpressApp1/model/expenseGroupUser.js
@@ -0,0 +1,28 @@
+﻿var connection = require('../data_repositories/connection');
+var Sequelize = require("sequelize");
+var user = require('./users');
+var expenseGroup = require('./expenseGroup');
+sequelize = connection.getConnection();
+
+var Models = {};
+const ExpenseGroupUsers = sequelize.define('ExpenseGroupUsers', {
+    GroupId: { type: Sequelize.INTEGER, allowNull: false, references: {
+            model: expenseGroup.ExpenseGroup,
+            key: 'GroupId',
+        }
+    },
+    UserId: {
+        type: Sequelize.INTEGER, 
+        allowNull: false, 
+        references: {
+            model: user.Users,
+            key: 'UserId',
+        }
+    }
+});
+ExpenseGroupUsers.sync().then(() => {
+// Table created
+console.log('Expense Group User table Created');
+});
+Models.ExpenseGroupUsers = ExpenseGroupUsers;
+module.exports = Models;
